f9825ea20806dd27d4ce24fb39649752d5d55a9f
Osiyomeoh <samuelaleonomoh5@gmail.com>
2024-08-31 13:49:15 +0100

    add all

--- ignition/modules/stakeERC20.ts
+++ ignition/modules/stakeERC20.ts
@@ -1,14 +1,14 @@
 
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
-
+// To deploy the staking contract for ERC20
 const StakingModule = buildModule("StakingModule", (m) => {
 
   const tokenAddress = m.getParameter<string>("tokenAddress", "");
   
  
 
-  const StakingContract = m.contract("tokenAddress", [tokenAddress] );
+  const StakingContract = m.contract("token", [tokenAddress] );
 
 
   return { StakingContract };
